966f3f2a257a5172d0e1c6988f67f36cd4b34cc8
charles Langdon <duecesr4men.lan8@gmail.com>
2023-05-15 00:44:25 -0600

    added a function submit button

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function submitlink(){
     window.location.href="https://github.com/KERBALlang/USAF-html-checkpoint/blob/8e903b74713919f419108a47d1f403ba37727382/index.html"
 }
+var button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+  alert("YOU Submitted your login...please use the home button to navigate.");
+}, true);
